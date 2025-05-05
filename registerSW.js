e8425c613df7c719ef3ce1388d45a13bdc946290
michelle.austin <michelle.austin@depop.com>
2025-05-05 22:27:24 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/thought-experiment-randomizer/sw.js', { scope: '/thought-experiment-randomizer/' })})}
